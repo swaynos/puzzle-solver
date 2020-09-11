6bd8066c41cba0db8c1b3edaa5b80e5a90101184
John Swaynos <john@swaynos.com>
2020-09-11 16:56:12 -0700

    Added docker configuration and attempting to connect to local dynamoDB

--- app.js
+++ app.js
@@ -10,6 +10,24 @@ const indexRouter = require('./routes/index');
 const registerRouter = require('./routes/register');
 const puzzleRouter = require('./routes/puzzle');
 
+// ##### ToDo: Test code
+const AWS = require('aws-sdk');
+// Set the region 
+//AWS.config.update({region: 'us-west-2'});
+
+// Create the DynamoDB service object
+const ddb = new AWS.DynamoDB({
+endpoint: new AWS.Endpoint('http://localhost:8000')
+});
+ddb.listTables({Limit: 10}, function(err, data) {
+  if (err) {
+    console.log("Error", err.code);
+  } else {
+    console.log("Table names are ", data.TableNames);
+  }
+});
+// ##### End Test Code
+
 // App initialization
 const app = express();
 
